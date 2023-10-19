f4e0c7efcb4414293c1ea03bd2e5975ce6555212
BenardOtokpa <benard.otokpa@yahoo.com>
2023-10-19 10:53:11 +0100

    Navbar creation

---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react'
+
+const Nav = () => {
+
+    const home = useRef(null)
+    const about = useRef(null)
+    const menu = useRef(null)
+    const testimonials = useRef(null)
+    
+    const scrollToSection = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: 'smooth',
+        })
+    }
+  return (
+    <nav >
+        <ul>
+        <li><a href='the'>Home</a></li>
+        <li><a href='fscrv'>About</a></li>
+        <li><a href='wrvwrv'>Menu</a></li>
+        <li><a href='wrv'>Reservations</a></li>
+        <li><a href='wvwrv'>Order Online</a></li>
+        <li><a href='wvr'>Testimonials</a></li>
+        <li><a href='wrv'>LogIn</a></li>
+    </ul>
+    </nav>
+  )
+}
+
+export default Nav
